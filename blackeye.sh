66acba784ca8b2fcb83cb8e968c8e1c746ede761
Yousha Ahmed <89526055+Yousha0311m@users.noreply.github.com>
2022-07-14 21:06:16 +0500

    Update blackeye.sh

--- blackeye.sh
+++ blackeye.sh
@@ -3,7 +3,7 @@
 # Coded by: @thelinuxchoice (https://github.com/thelinuxchoice/blackeye)
 # Upgraded by: @suljot_gjoka (https://github.com/whiteeagle0/blackeye)
 
-import webbrowser
+
 
 trap 'printf "\n";stop;exit 1' 2
 
@@ -41,7 +41,6 @@ read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m\en' op
 
 
 if [[ $option == 1 ]]; then
-webbrowser.open('http://example.com')
 server="instagram"
 start
 
